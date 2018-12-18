8f0846e24c8d655e8884fb634df93d333b3ae614
yuichikato_ <k.yuichi123@gmail.com>
2018-12-18 09:36:31 +0000

    Finish user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 SamuraiApp::Application.routes.draw do
-  root  'static_pages#home'
-  
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
   resources :users, only: [:show]
-  devise_for  :users
+  root  'static_pages#home'
   match '/help',  to:'static_pages#help',  via:'get'
   match '/about',  to:'static_pages#about',  via:'get'
   match '/contact',  to:'static_pages#contact',  via:'get'
